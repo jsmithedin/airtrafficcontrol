b5a1ba43a6c6a377776b5d2b11ce316004bbecac
Jamie Smith <jamie@jsmth.co.uk>
2019-10-16 20:54:37 +0100

    Create own test input

--- deploy_test.go
+++ deploy_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -32,18 +33,28 @@ func TestRunCmd(t *testing.T) {
 }
 
 func TestLoadConfig(t *testing.T) {
+	testCfg := deployConfig{RepositoryURL: "https://github.com/kelseyhightower/nocode.git", BuildCMD: "echo build", InstallCMD: "echo install"}
+	configFilePath, _ := ioutil.TempFile("", "config")
+	ymlBytes, _ := yaml.Marshal(testCfg)
+	_ = ioutil.WriteFile(configFilePath.Name(), ymlBytes, 0644)
+	defer os.Remove(configFilePath.Name())
+
 	var dCfg deployConfig
-	configFilePath := "test.yml"
-	dCfg.loadConfig(configFilePath)
+	dCfg.loadConfig(configFilePath.Name())
 	if dCfg.BuildCMD != "echo build" || dCfg.InstallCMD != "echo install" || dCfg.RepositoryURL != "https://github.com/kelseyhightower/nocode.git" {
 		t.Errorf("Didn't load config properly")
 	}
 }
 
 func TestDeploy(t *testing.T) {
+	testCfg := deployConfig{RepositoryURL: "https://github.com/kelseyhightower/nocode.git", BuildCMD: "echo build", InstallCMD: "echo install"}
+	configFilePath, _ := ioutil.TempFile("", "config")
+	ymlBytes, _ := yaml.Marshal(testCfg)
+	_ = ioutil.WriteFile(configFilePath.Name(), ymlBytes, 0644)
+	defer os.Remove(configFilePath.Name())
+
 	var dCfg deployConfig
-	configFilePath := "test.yml"
-	dCfg.loadConfig(configFilePath)
+	dCfg.loadConfig(configFilePath.Name())
 	err := runDeploy(&dCfg)
 	if err != nil {
 		t.Errorf("Didn't deploy: %s", err)
